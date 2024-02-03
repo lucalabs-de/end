fc0f6d06804149e3bc763b657f0ce50fea10dd4c
lucalabs-de <gspitzer@leoapp-slg.de>
2024-02-03 21:57:51 +0100

    fixed performance issues

--- app/Util/ImageConversion.hs
+++ app/Util/ImageConversion.hs
@@ -2,9 +2,13 @@ module Util.ImageConversion where
 
 import Codec.Picture (PixelRGBA8 (PixelRGBA8), generateImage, writePng)
 import DBus (arrayItems, fromVariant)
+import Data.Array (listArray, (!))
+import qualified Data.Array as DA
 import Data.Maybe (fromMaybe)
 import Data.Word (Word8)
 import Util.DbusNotify (ImageData)
+
+type Array v = DA.Array Int v -- int-indexed array
 
 writeImageDataToPng :: ImageData -> IO FilePath
 writeImageDataToPng img = do
@@ -17,20 +21,25 @@ writeImageDataToPng img = do
   let stride = fromIntegral strideData
 
   let pixels = fromMaybe 0 . fromVariant <$> arrayItems pixelData :: [Word8]
+  
+  -- transform list to array to ensure constant indexed access
+  let pixelArray = listArray (0, length pixels - 1) pixels
 
-  writePng path $ generateImage (pixelTransformer stride hasAlpha pixels) width height
+  writePng path $ generateImage (pixelTransformer stride hasAlpha pixelArray) width height
   putStrLn "done"
   return path
 
-pixelTransformer :: Int -> Bool -> [Word8] -> Int -> Int -> PixelRGBA8
-pixelTransformer stride hasAlpha pixels x y =
-  if hasAlpha
-    then PixelRGBA8 r g b a
-    else PixelRGBA8 r g b 255
+pixelTransformer :: Int -> Bool -> Array Word8 -> Int -> Int -> PixelRGBA8
+pixelTransformer stride hasAlpha pixels x y
+  | length pixels > pixelIndex =
+      if hasAlpha
+        then PixelRGBA8 r g b a
+        else PixelRGBA8 r g b 255
+  | otherwise = PixelRGBA8 0 0 0 0
  where
   pixelSize = if hasAlpha then 4 else 3
   pixelIndex = y * stride + x * pixelSize
-  r = pixels !! pixelIndex
-  g = pixels !! (pixelIndex + 1)
-  b = pixels !! (pixelIndex + 2)
-  a = pixels !! (pixelIndex + 3)
+  r = pixels ! pixelIndex
+  g = pixels ! (pixelIndex + 1)
+  b = pixels ! (pixelIndex + 2)
+  a = pixels ! (pixelIndex + 3)
